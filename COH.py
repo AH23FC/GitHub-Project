900a221ea95ae979a8348bd72018e33cb42340df
AH23FC <121472381+AH23FC@users.noreply.github.com>
2023-02-01 11:56:44 +0800

    Update COH.py

--- COH.py
+++ COH.py
@@ -1,1 +1,6 @@
-
+from pathlib import Path
+import csv
+# Referencing Cash On Hand.csv from csv_report folder
+fp = Path(r"C:\csv_reports\Cash on Hand.csv")
+# Read the CSV file to append rofit and quantity from the csv
+with fp.open(
